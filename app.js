7961798a2650de496c6bc5668e7017ed88ca570a
Lexus Banton <lexusbanton.kr@gmail.com>
2022-09-15 20:32:53 -0700

    it was single quotes!

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 /* Imports */
-
+const climateSelect = document.getElementById('climate-select');
+const architectureSelect = document.getElementById('architecture-select');
+const attractionsSelect = document.getElementById('attractions-select');
+const climateImage = document.getElementById('climate-image');
+const ArchitectureImage = document.getElementById('architecture-image');
+const attractionsImage = document.getElementById('attractions-image');
+const sloganInput = document.getElementById('slogan-input');
+const addButton = document.getElementById('add-button');
+const phraseList = document.getElementById('phrase-list');
 /* Get DOM Elements */
 
 /* State */
+const destination = {
+    climate: 'tropical',
+    architecture: 'city',
+    attractions: 'shrine',
+    slogan: [],
+};
 
 /* Events */
 
