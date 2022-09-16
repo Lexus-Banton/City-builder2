f091644eefe1a38c153dc3aee0beb1467d84a818
Lexus Banton <lexusbanton.kr@gmail.com>
2022-09-16 12:49:43 -0700

    fixed slogan area

--- app.js
+++ app.js
@@ -7,18 +7,26 @@ const architectureImage = document.getElementById('architecture-image');
 const attractionsImage = document.getElementById('attractions-image');
 const sloganInput = document.getElementById('slogan-input');
 const addButton = document.getElementById('add-button');
-const phraseList = document.getElementById('phrase-list');
+const sloganList = document.getElementById('slogan-list');
+const nameInput = document.getElementById('name-input');
+const nameList = document.getElementById('name-list');
 /* Get DOM Elements */
 
 /* State */
 const destination = {
+    names: [],
     climate: 'tropical',
     architecture: 'city',
     attractions: 'shrine',
-    slogan: [],
+    slogans: [],
 };
 
 /* Events */
+nameInput.addEventListener('input', () => {
+    destination.name = nameInput.value;
+
+    displayDestination();
+});
 climateSelect.addEventListener('change', () => {
     destination.climate = climateSelect.value;
     displayDestination();
@@ -40,4 +48,36 @@ function displayDestination() {
     attractionsImage.src = 'assets/city/' + destination.attractions + '-attractions.png';
 }
 
+function displaySlogans() {
+    sloganList.innerHTML = '';
+
+    for (const slogan of destination.slogans) {
+        const li = document.createElement('li');
+        li.textContent = slogan;
+        sloganList.append(li);
+    }
+}
+
+addButton.addEventListener('click', () => {
+    const slogan = sloganInput.value;
+    if (slogan === '') {
+        return;
+    }
+    destination.slogans.push(slogan);
+    displaySlogans();
+
+    sloganInput.value = '';
+});
+//below does not work gosh darn it
+function displayName() {
+    nameInput.innerHTML = '';
+    for (const name of destination.names) {
+        const p = document.createElement('p');
+        p.textContent = name;
+        nameList.append(p);
+    }
+}
+displayDestination();
+displaySlogans();
+displayName();
 // (don't forget to call any display functions you want to run on page load!)
