0e5b61e15e1d9a936ef13297efe43353e5b7a6bf
Lexus Banton <lexusbanton.kr@gmail.com>
2022-09-16 09:52:15 -0700

    fixed images

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const climateSelect = document.getElementById('climate-select');
 const architectureSelect = document.getElementById('architecture-select');
 const attractionsSelect = document.getElementById('attractions-select');
 const climateImage = document.getElementById('climate-image');
-const ArchitectureImage = document.getElementById('architecture-image');
+const architectureImage = document.getElementById('architecture-image');
 const attractionsImage = document.getElementById('attractions-image');
 const sloganInput = document.getElementById('slogan-input');
 const addButton = document.getElementById('add-button');
@@ -35,7 +35,9 @@ attractionsSelect.addEventListener('change', () => {
 });
 /* Display Functions */
 function displayDestination() {
-    climateImage.src = '/assets';
+    climateImage.src = '/assets/city/' + destination.climate + '-climate.png';
+    architectureImage.src = 'assets/city/' + destination.architecture + '-architecture.png';
+    attractionsImage.src = 'assets/city/' + destination.attractions + '-attractions.png';
 }
 
 // (don't forget to call any display functions you want to run on page load!)
